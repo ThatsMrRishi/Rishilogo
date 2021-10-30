6fbe6d8af65c1b25e6fdc7a8b04f21e64e09f537
CalmedRishi <76921702+CalmedRishi@users.noreply.github.com>
2021-10-30 10:55:04 -0400

    Create logo.py
    this is a file that preprs logos

---
+++ logo.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+import turtle
+t.fillcolor("Yellow")
+t=turtle.Turtle()
+
+for i in range(5):
+    t.forward(150)
+    t.right(144)
+
+turtle.write("Star Wars", font=("Arial", 16, "normal"))
+turtle.done()
